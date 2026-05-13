41e788aa68338b985c1e52ec19e54f50f5e15692
67hp5tbvkr-blip <67hp5tbvkr@privaterelay.appleid.com>
2026-05-13 14:42:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-console.log("JS CHARGÉ OK");
+const videos = [
+  "G3zP-RhcgAE",
+  "FT0frI2LMtY",
+  "fLNfS5OR8t4",
+  "3m3XVDgL7ww",
+  "nfFwveM2eLA"
+];
 
 const grid = document.getElementById("grid");
 const lightbox = document.getElementById("lightbox");
 const player = document.getElementById("player");
 
-const videos = [
-  "G3zP-RhcgAE",
-  "FT0frI2LMtY",
-  "fLNfS5OR8t4"
-];
+function openVideo(id){
+  lightbox.style.display = "flex";
+  player.src = `https://www.youtube.com/embed/${id}?autoplay=1&mute=1&rel=0`;
+}
+
+function closeVideo(){
+  lightbox.style.display = "none";
+  player.src = "";
+}
+
+document.getElementById("close").onclick = closeVideo;
 
 videos.forEach(id => {
   const div = document.createElement("div");
+  div.className = "card";
+
   div.innerHTML = `
     <img src="https://img.youtube.com/vi/${id}/hqdefault.jpg">
   `;
 
-  div.onclick = () => {
-    lightbox.style.display = "block";
-    player.src = `https://www.youtube.com/embed/${id}?autoplay=1&mute=1`;
-  };
+  div.onclick = () => openVideo(id);
 
   grid.appendChild(div);
 });
-
-document.getElementById("close").onclick = () => {
-  lightbox.style.display = "none";
-  player.src = "";
-};  "fLNfS5OR8t4",
-  "3m3XVDgL7ww",
-  "nfFwveM2eLA"
-];
-
-fallbackVideos.forEach(addVideo);
